df84b20e16bc966bb06dd6684092e8c5385e1674
QianruZhang78 <zhangqianru020708@gmail.com>
2020-03-20 19:20:51 +0800

    merge sort

--- src/algorithms/Sorting.java
+++ src/algorithms/Sorting.java
@@ -17,11 +17,42 @@ public class Sorting {
         ArrayList<Integer> result = new ArrayList<>();
 
         /* YOUR CODE HERE */
-
+        int lengthA = a.size();
+        int lengthB = b.size();
+        int currentA = 0;
+        int currentB = 0;
+        while (currentA < lengthA && currentB < lengthB) {
+            if (a.get(currentA) <= b.get(currentB)) {
+                result.add(a.get(currentA));
+                currentA++;
+            } else {
+                result.add(b.get(currentB));
+                currentB++;
+            }
+        }
+        while (currentA < lengthA) {
+            result.add(a.get(currentA));
+            currentA++;
+        }
+        while (currentB < lengthB) {
+            result.add(b.get(currentB));
+            currentB++;
+        }
         return result;
     }
 
     public static ArrayList<Integer> mergeSort(ArrayList<Integer> list) {
         /* YOUR CODE HERE */
+        if (list == null) {
+            return null;
+        }
+        int length = list.size();
+        if (length == 0 || length == 1) {
+            return list;
+        }
+        int mid = length / 2;
+        ArrayList<Integer> left = mergeSort(new ArrayList<>(list.subList(0, mid)));
+        ArrayList<Integer> right = mergeSort(new ArrayList<>(list.subList(mid, length)));
+        return merge(left, right);
     }
 }
