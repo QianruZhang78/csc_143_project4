0823323356cdc5954ea7cba4eba9f4800439d445
QianruZhang78 <zhangqianru020708@gmail.com>
2020-03-20 19:13:50 +0800

    initialize

---
+++ src/algorithms/Sorting.java
@@ -0,0 +1,27 @@
+package algorithms;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Sorting {
+    /**
+     * Merge two ArrayLists efficiently, in ascending order. Does not modify either argument.
+     * @param a The first list.
+     * @param b The second list.
+     * @return A new sorted ArrayList containing elements from both lists.
+     */
+    public static ArrayList<Integer> merge(ArrayList<Integer> a, ArrayList<Integer> b) {
+        ArrayList<Integer> result = new ArrayList<>();
+
+        /* YOUR CODE HERE */
+
+        return result;
+    }
+
+    public static ArrayList<Integer> mergeSort(ArrayList<Integer> list) {
+        /* YOUR CODE HERE */
+    }
+}
