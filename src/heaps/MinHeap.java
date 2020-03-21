5e53d33e8bc283250e515860ee9eefe2bce5292f
QianruZhang78 <zhangqianru020708@gmail.com>
2020-03-21 10:53:40 +0800

    MinHeap

--- src/heaps/MinHeap.java
+++ src/heaps/MinHeap.java
@@ -11,8 +11,8 @@ import java.util.NoSuchElementException;
  * formulas discussed in class to retrieve and assign child nodes.
  *
  * @param <T> The generic type stored in the heap. Must be a Comparable type.
- *           For more information on the Comparable interface, please read:
- *           https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/lang/Comparable.html
+ *            For more information on the Comparable interface, please read:
+ *            https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/lang/Comparable.html
  */
 public class MinHeap<T extends Comparable<T>> {
     public ArrayList<T> container = new ArrayList<>();
@@ -46,23 +46,77 @@ public class MinHeap<T extends Comparable<T>> {
 
     /**
      * Inserts a value into the heap, "bubbling up" to the correct position.
+     *
      * @param value The value to be inserted.
      */
     public void insert(T value) {
         /* YOUR CODE HERE */
+        container.add(value);
+        int index = container.size() - 1;
+        while (getParentIndex(index) > 0 && container.get(getParentIndex(index)).compareTo(value) > 0) {
+            swap(index, getParentIndex(index));
+            index = getParentIndex(index);
+        }
+
     }
 
     public T peek() {
         /* YOUR CODE HERE */
-        return null;
+        if (container.size() == 1) {
+            throw new NoSuchElementException("no elements in the heap");
+        }
+        return container.get(1);
     }
 
     public T remove() {
         /* YOUR CODE HERE */
-        return null;
+        if (container.size() == 1) {
+            throw new NoSuchElementException("no elements in the heap");
+        }
+        T result = container.get(1);
+        container.set(1, container.get(container.size() - 1));
+        container.remove(container.size() - 1);
+        int index = 1;
+        int leftIndex = getLeftChildIndex(index);
+        int rightIndex = getRightChildIndex(index);
+        while (leftIndex < container.size()) {
+            T left = container.get(leftIndex);
+            T right = null;
+            if (rightIndex < container.size()) {
+                right = container.get(rightIndex);
+            }
+            if (isLeftSmallerThanRight(left, right)) {
+                if (container.get(index).compareTo(container.get(leftIndex)) > 0) {
+                    swap(index, leftIndex);
+                    index = leftIndex;
+
+                } else {
+                    break;
+                }
+            } else {
+                if (container.get(index).compareTo(container.get(rightIndex)) > 0) {
+                    swap(index, rightIndex);
+                    index = rightIndex;
+                } else {
+                    break;
+                }
+            }
+            leftIndex = getLeftChildIndex(index);
+            rightIndex = getRightChildIndex(index);
+
+        }
+        return result;
+    }
+
+    private boolean isLeftSmallerThanRight(T left, T right) {
+        if (right == null) {
+            return true;
+        }
+        return left.compareTo(right) <= 0;
     }
 
     public int size() {
         /* YOUR CODE HERE */
+        return container.size() - 1;
     }
 }
